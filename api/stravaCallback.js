1fd3759218ad59d103d4e4dffd5424fbb1e52350
dyeraandirana <135441436+dyeraandirana@users.noreply.github.com>
2025-09-01 14:30:14 +0700

    Update stravaCallback.js

--- api/stravaCallback.js
+++ api/stravaCallback.js
@@ -1,7 +1,14 @@
 import fetch from "node-fetch";
 import { google } from "googleapis";
 
+function formatPrivateKey(key) {
+  return key.replace(/\
+/g, "
+");
+}
+
 export default async function handler(req, res) {
+  console.log("Callback query:", req.query);
   const code = req.query.code;
   const userId = req.query.state;
   if (!code || !userId) return res.status(400).send("Missing code or userId");
@@ -18,6 +25,7 @@ export default async function handler(req, res) {
       })
     });
     const data = await response.json();
+    console.log("Strava token response:", data);
     const { access_token, refresh_token, expires_at, athlete } = data;
 
     if (!access_token) {
@@ -28,7 +36,7 @@ export default async function handler(req, res) {
     const auth = new google.auth.JWT(
       process.env.GOOGLE_CLIENT_EMAIL,
       null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      formatPrivateKey(process.env.GOOGLE_PRIVATE_KEY),
       ["https://www.googleapis.com/auth/spreadsheets"]
     );
     const sheets = google.sheets({ version: "v4", auth });
